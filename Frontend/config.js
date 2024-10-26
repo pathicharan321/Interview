cad67efca3194dcb7f3ca2de92cdf2d44aafa618
pathicharan321 <138883958+pathicharan321@users.noreply.github.com>
2024-10-27 01:29:19 +0530

    Update config.js url

--- Frontend/config.js
+++ Frontend/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-    API_URL: 'https://interview-backend-ydot.onrender.com'
+    API_URL: 'https://interview-backend-qqij.onrender.com'
 }
 export default config;
   
